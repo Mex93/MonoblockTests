b15f0fa1718e2b0e74bb87219f3e0f040bc10379
Mex93 <ryasanow@mail.ru>
2024-10-06 21:58:37 +0300

    hardkeys

--- ui/get_check_string_window.py
+++ ui/get_check_string_window.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'get_check_string_window.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.7.2
+## Created by: Qt User Interface Compiler version 6.7.3
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,15 +15,16 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QSizePolicy,
-    QTextBrowser, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QSpacerItem, QTextBrowser,
+    QVBoxLayout, QWidget)
 import ui.res_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(749, 479)
+        MainWindow.resize(622, 390)
         icon = QIcon()
         icon.addFile(u":/res/images/logo.ico", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
         MainWindow.setWindowIcon(icon)
@@ -44,6 +45,20 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addWidget(self.label)
 
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+        self.pushButton_set_config = QPushButton(self.centralwidget)
+        self.pushButton_set_config.setObjectName(u"pushButton_set_config")
+
+        self.horizontalLayout.addWidget(self.pushButton_set_config)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
         self.textBrowser_set_string = QTextBrowser(self.centralwidget)
         self.textBrowser_set_string.setObjectName(u"textBrowser_set_string")
 
@@ -62,5 +77,6 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"\u0421\u0442\u0440\u043e\u043a\u0438 \u0434\u043b\u044f \u043f\u0440\u043e\u0432\u0435\u0440\u043a\u0438:", None))
+        self.pushButton_set_config.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043a\u043e\u043f\u0438\u0440\u043e\u0432\u0430\u0442\u044c \u0432 \u043a\u043e\u043d\u0444\u0438\u0433", None))
     # retranslateUi
 
